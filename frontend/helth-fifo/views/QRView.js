4b37a4d8318332e24399886a61857e32fb275126
dawidw_ <dawidw_@outlook.com>
2023-09-30 19:20:40 +0200

    Add QRView

---
+++ frontend/helth-fifo/views/QRView.js
@@ -0,0 +1,126 @@
+import { StyleSheet, Styles, Text, Alert, View, ScrollView, TouchableOpacity } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import TitleBar from '../components/TitleBar';
+import ActionButton from '../components/ActionButton';
+import GlobalStyles from '../Styles'
+import React, { useState, useEffect } from 'react';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { Camera } from 'expo-camera';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function QRView({ navigation }) {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }, []);
+
+    const handleBarCodeScanned = ({ type, data }) => {
+        setScanned(true);
+        // alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+        Alert.alert('Qr kod nowy', 'Mamy nowy qr-kod', [
+            {
+                text: 'Dodaj',
+                onPress: () => {
+                    console.log(`Dodajemy ${data}`)
+                    setScanned(false)
+                },
+                style: 'cancel',
+            },
+            {
+                text: 'Anuluj', onPress: () => {
+                    console.log(`Anuluj ${data}`)
+                    setScanned(false)
+                }
+            },
+        ]);
+
+    };
+
+    const renderCamera = () => {
+        return (
+            <View style={styles.cameraContainer}>
+                <BarCodeScanner
+                    onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                    style={styles.camera}
+                />
+            </View>
+        );
+    };
+
+    if (hasPermission === null) {
+        return <View />;
+    }
+
+    if (hasPermission === false) {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>Camera permission not granted</Text>
+            </View>
+        );
+    }
+    SplashScreen.hideAsync();
+
+    return (
+        <View style={GlobalStyles.container}>
+            <TitleBar subtitle="QR skaner" title="Dodaj kolejke" hideBackBtn={true} />
+            <View style={styles.container}>
+                {renderCamera()}
+            </View>
+            <ActionButton text="Skanuj kod wizyty" callback={() => {
+                setScanned(false);
+                // Dodaj wizyte pewnie lepiej
+                // navigation.navigate('CreateWorkout')
+            }}></ActionButton>
+        </View >
+    );
+}
+
+const styles = StyleSheet.create({
+    visitElement: {
+        marginHorizontal: 20,
+        borderBottomColor: "#E4E4E4",
+        borderBottomWidth: 4,
+        paddingVertical: 6
+    },
+    midText: {
+        fontSize: 20,
+        fontFamily: 'mrt-bold',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    paragraph: {
+        fontSize: 16,
+        marginBottom: 40,
+    },
+    cameraContainer: {
+        width: '80%',
+        aspectRatio: 1,
+        overflow: 'hidden',
+        borderRadius: 10,
+        marginBottom: 40,
+    },
+    camera: {
+        flex: 1,
+    },
+    button: {
+        backgroundColor: 'blue',
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+
+});
