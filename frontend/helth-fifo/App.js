0e70065414bb1caca644f37191ed6613b3fb818a
dawidw_ <dawidw_@outlook.com>
2023-09-30 15:40:07 +0200

    Add fonts

--- frontend/helth-fifo/App.js
+++ frontend/helth-fifo/App.js
@@ -1,10 +1,21 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './views/Home'
+import { useFonts } from 'expo-font';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [isLoaded] = useFonts({
+    "mrt-mid": require("./assets/fonts/Montserrat/static/Montserrat-Medium.ttf"),
+    "mrt-bold": require("./assets/fonts/Montserrat/static/Montserrat-Bold.ttf"),
+    "mrt-xbold": require("./assets/fonts/Montserrat/static/Montserrat-ExtraBold.ttf"),
+  });
+
+  if (!isLoaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{
